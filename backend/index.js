5d7fdff0ac4a2e834de8e3817ac54610615f4574
Jaiharan <136414923+Jaiharan@users.noreply.github.com>
2024-02-04 17:23:08 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,16 @@ app.get("/books",(req,res)=>{
   });
 });
 
+// to get data of single book using bookid
+app.get("/books/:id",(req,res)=>{
+  const bookId = req.params.id;
+  const q = "SELECT * FROM books WHERE id = ?"
+  db.query(q,[bookId],(err,data)=>{
+    if(err) return res.json(err)
+    return res.json(data);
+  });
+});
+
 app.post("/books",(req,res)=>{
   const q = "INSERT INTO books (`title`,`desc`,`price`,`cover`) VALUES (?)"
   const values = [
